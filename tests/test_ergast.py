a691e5570ed42f2701025d1b20c40f89cd86c772
meepheep <nolanc805@gmail.com>
2026-09-03 17:33:07 -0500

    Update test_ergast.py

--- tests/test_ergast.py
+++ tests/test_ergast.py
@@ -4,10 +4,6 @@ import requests
 ergast = Ergast(result_type="pandas", auto_cast=True)
 version = "1.0.0"
 
-#results = ergast.get_race_results(season=2024, round=1)
+results = ergast.get_race_results(season=2024, round=1)
 
-results = requests.get(
-    "https://api.jolpi.ca/ergast/f1/2026/races/?format=json",
-    headers={"User-Agent": f"YourAppName/{version}"},
-)
-print(results.json()["MRData"]['RaceTable']['Races'][0]['Circuit']['circuitName'])
+print(results)

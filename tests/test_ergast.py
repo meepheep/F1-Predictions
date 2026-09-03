ebb56704d239ff0c2761a96d2faa6fab9d1b49bd
meepheep <nolanc805@gmail.com>
2026-09-03 17:08:26 -0500

    updated tests/test_ergast.py to use requests.get to fetch data from the Ergast API with a custom User-Agent header.

--- tests/test_ergast.py
+++ tests/test_ergast.py
@@ -1,7 +1,13 @@
 from fastf1.ergast import Ergast
-import src.config
+import requests
 
 ergast = Ergast(result_type="pandas", auto_cast=True)
+version = "1.0.0"
 
-results = ergast.get_race_results(season=2024, round=1)
-print(results.content[0].head())
+#results = ergast.get_race_results(season=2024, round=1)
+
+results = requests.get(
+    "https://api.jolpi.ca/ergast/f1/2026/races/?format=json",
+    headers={"User-Agent": f"YourAppName/{version}"},
+)
+print(results.json()["MRData"]['RaceTable']['Races'][0]['Circuit']['circuitName'])
